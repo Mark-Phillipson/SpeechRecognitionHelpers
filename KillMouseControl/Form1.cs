2cc65f41e93ead30ca8b6d5de1aeebfdd2af196f
MPhil <MPhillipson0@Gmail.com>
2019-04-07 16:56:22 +0100

    Mouse Control

---
+++ KillMouseControl/Form1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KillMouseControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var processes= Process.GetProcessesByName("MouseControl");
+            foreach (var process in processes)
+            {
+                process.Kill();
+            }
+            Application.Exit();
+        }
+    }
+}
---
+++ MouseControl/MouseControlForm.Designer.cs
@@ -0,0 +1,48 @@
+namespace MouseControl
+{
+    partial class MouseControlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MouseControlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MouseControlForm";
+            this.Text = "Mouse Control";
+            this.Load += new System.EventHandler(this.MouseControlForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
