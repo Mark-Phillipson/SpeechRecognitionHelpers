bece735f7a114adb76120e5d655439c8dd9f8370
MPhil <MPhillipson0@Gmail.com>
2019-09-29 15:42:28 +0100

    Dictation Box no longer tries to paste an image

--- ExecuteCommands/Commands.cs
+++ ExecuteCommands/Commands.cs
@@ -20,7 +20,7 @@ namespace ExecuteCommands
             string[] args = Environment.GetCommandLineArgs();
             if (args.Count() < 2)
             {
-                arguments = new string[] { args[0], "CloseFileExplorer" };
+                arguments = new string[] { args[0], "ShowDictationBox" };
                 //arguments=  new string[] { args[0], "/upper-left" };
             }
             else
@@ -31,6 +31,11 @@ namespace ExecuteCommands
             {
                 CloseFileExplorer();
                 Console.WriteLine("Close File Explorer Ran successfully. ");
+            }
+            else if (arguments[1]=="ShowDictationBox")
+            {
+                ShowDictationBox();
+                Console.WriteLine("The Show Dictation Command ran successfully!");
             }
             else
             {
@@ -47,6 +52,20 @@ namespace ExecuteCommands
             foreach (var process in processes)
             {
                 process.Kill();
+            }
+        }
+        private void ShowDictationBox()
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                //This is where we can launch the dictation box
+                var filename = @"C:\Program Files (x86)\MSP Systems\Speech Recognition Helpers\DictationBoxMSP.exe";
+                Process process = new Process();
+                process.StartInfo.UseShellExecute = true;
+                //process.StartInfo.WorkingDirectory = "C:\\Program Files (x86)\\KnowBrainer\\KnowBrainer Professional 2017\\";
+                process.StartInfo.FileName = filename;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                process.Start();
             }
         }
     }
