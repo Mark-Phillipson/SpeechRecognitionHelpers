686a48f95a81315eec59aad6e3479dfa97c6420b
Mark Phillipson <MPhillipson0@Gmail.com>
2020-02-18 20:24:45 +0000

    Add files via upload

---
+++ BrowseScripts/Publish/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowseScripts.Publish
+{
+    class Class1
+    {
+        public int Id { get; set; }
+    }
+}
