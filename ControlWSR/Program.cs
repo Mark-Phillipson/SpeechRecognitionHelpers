53f92c44b750a7fdaa85589437514cf9e3fa193e
Mark Phillipson <MPhillipson0@Gmail.com>
2020-12-14 09:05:53 +0000

    Removal of Cosmos DB

--- ControlWSR/Program.cs
+++ ControlWSR/Program.cs
@@ -8,7 +8,7 @@ namespace ControlWSR
 {
 	static class Program
 	{
-		/// <summary>
+		/// <summary>New Foxtrot one
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
@@ -17,16 +17,6 @@ namespace ControlWSR
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new AvailableCommandsForm());
-			//Console.WriteLine("Azure Cosmos Table Samples");
-			//BasicSamples basicSamples = new BasicSamples();
-			//basicSamples.RunSamples().Wait();
-
-			//AdvancedSamples advancedSamples = new AdvancedSamples();
-			//advancedSamples.RunSamples().Wait();
-
-			//Console.WriteLine();
-			//Console.WriteLine("Press any key to exit");
-			//Console.Read();
 
 		}
 	}
